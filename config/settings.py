f2307fa3ccc41aadc59b49d86f16e093d2b314f3
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-13 19:09:21 +0100

    feature: Add Heroku settings

--- config/settings.py
+++ config/settings.py
@@ -19,10 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tv=!2jr25+t0w!6(0bs^u-fyq38ij))s54=a5%)#j+cjd))d%e'
-
+# SECRET_KEY = 'django-insecure-tv=!2jr25+t0w!6(0bs^u-fyq38ij))s54=a5%)#j+cjd))d%e'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-tv=!2jr25+t0w!6(0bs^u-fyq38ij))s54=a5%)#j+cjd))d%e')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +129,10 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
